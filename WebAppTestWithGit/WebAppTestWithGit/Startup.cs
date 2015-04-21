21aadb34d768cb8083d821cce8e7190936785960
Andreas Gidö <andreas.gido@gmail.com>
2015-04-21 09:28:09 +0200

    Nytt projekt!

---
+++ WebAppTestWithGit/WebAppTestWithGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTestWithGit.Startup))]
+namespace WebAppTestWithGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
