4d059eba729204d62f53804d6661c52a041e332e
Andreas Gidö <andreas.gido@gmail.com>
2015-04-22 15:41:24 +0200

    text, login borta

--- WebAppTestWithGit/WebAppTestWithGit/Controllers/HomeController.cs
+++ WebAppTestWithGit/WebAppTestWithGit/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace WebAppTestWithGit.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Om denna site.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Kontaktuppgifter.";
 
             return View();
         }
